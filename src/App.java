cfa28968e3b9bbf0e10530feb2a36ba85f24a160
Edwyn Guzman <edwynjesus30@gmail.com>
2024-08-06 11:43:22 -0400

    reset-desktop

--- src/App.java
+++ src/App.java
@@ -1,11 +1,6 @@
-import  main.view.*;
-
 public class App {
-    public static void main(String[] args) {
-
-        System.out.println("Bienvenidos a course stack");
-        new LoginView().setVisible(true);
-        
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+ 
     }
-
 }
